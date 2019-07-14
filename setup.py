07293f9c2f1b9eef05b6b3bb642f4007101b823f
npmitchell <npmitchell@kitp.ucsb.edu>
2019-07-13 18:27:20 -0700

    whitespace changes in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='basics',
     version='0.0',
     description='Basic Python Modules',
-#      url='needs a URL',
+    # url='needs a URL',
     author='Noah Mitchell',
     author_email='noah.prentice.mitchell@gmail.com',
     license='MIT',
